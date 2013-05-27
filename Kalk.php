d778231741d87afcaca57ce366d3b80f3fc881a8
YulyaS <yulya.sahno@bk.ru>
2013-05-27 09:34:44 +0300

    Update and rename Kalk.htm to Kalk.php

--- Kalk.php
+++ Kalk.php
@@ -1,4 +1,10 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+
+<html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Шкафы</title>
 <script language="JavaScript">
 function touch()
